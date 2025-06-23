ff0aa8d2a19ff511dbc03a214e22cca25911b640
Gami <s1320016@u-aizu.ac.jp>
2025-06-23 13:26:19 +0900

    fix:copy bat

--- copy.bat
+++ copy.bat
@@ -1,6 +1,6 @@
 @echo off
 set SOURCE=C:\programming\minecraft_addon_develop\rouge\rouge_function_behavior
-set DEST=C:\Users\nakah\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs
+set DEST=C:\Users\nakah\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs\rouge_function_behavior
 
 xcopy "%SOURCE%" "%DEST%" /E /I /Y
 
